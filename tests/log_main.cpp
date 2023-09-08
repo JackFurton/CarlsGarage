4a0122e38fd4efec2d16c1e14f7f0ed4575bbf53
JackFUrton <36176901+JackFUrton@users.noreply.github.com>
2023-09-08 01:56:31 +0000

    broken quiet test

--- tests/log_main.cpp
+++ tests/log_main.cpp
@@ -24,54 +24,44 @@ BOOST_AUTO_TEST_SUITE(LoggerSuite)
 /** Important: getopt maintains global state.  Between tests you need to 
 *   reset optind or you will get strange and unexpected behavior.
 */
-BOOST_AUTO_TEST_CASE(test_getopt_parse_log_level_debug) 
+BOOST_AUTO_TEST_CASE(test_getopt_parse_log_level) 
 {
     optind = 1;
     log_global_cfg.level_cli_override = false;
-    BOOST_TEST_MESSAGE("STARTING CASE 1");
-    char *fake_argv[] = { (char*)"woof", (char*)"-l", (char*)"debug"};
-    int fake_argc = sizeof(fake_argv) / sizeof(char*);
-    parse_args(fake_argc, fake_argv);
+    BOOST_TEST_MESSAGE("STARTING CASE 1 DEBUG");
+    char *fake_debugv[] = { (char*)"woof", (char*)"-l", (char*)"debug"};
+    int fake_debugc= sizeof(fake_debugv) / sizeof(char*);
+    parse_args(fake_debugc, fake_debugv);
     BOOST_CHECK_EQUAL(log_global_cfg.level, LOG_DEBUG);
 
-    /*
-        #TODO JF: This test should pass but does not, plox fix
-    */
     BOOST_CHECK_EQUAL(log_global_cfg.level_cli_override, true);
-    /*
-        #TODO JF: when you fix this, its expected that it breaks 
-        the asserts in the following tests for log_level checks.
-        so try to fix those.
 
-    */
-
-}
-
-BOOST_AUTO_TEST_CASE(test_getopt_parse_log_level_info) 
-{
     optind = 1;
     log_global_cfg.level_cli_override = false;
-    BOOST_TEST_MESSAGE("STARTING CASE 2");
-    char *fake_argv[] = { (char*)"woof", (char*)"-l", (char*)"info"};
-    int fake_argc = sizeof(fake_argv) / sizeof(char*);
-    parse_args(fake_argc, fake_argv);
+    BOOST_TEST_MESSAGE("STARTING CASE 2 INFO");
+    char *fake_infov[] = { (char*)"woof", (char*)"-l", (char*)"info"};
+    int fake_infoc= sizeof(fake_infov) / sizeof(char*);
+    parse_args(fake_infoc, fake_infov);
 
     BOOST_CHECK_EQUAL(log_global_cfg.level, LOG_INFO);
 
-
-}
-
-BOOST_AUTO_TEST_CASE(test_getopt_parse_log_level_error) 
-{
     optind = 1;
     log_global_cfg.level_cli_override = false;
-    BOOST_TEST_MESSAGE("STARTING CASE 3");
-    char *fake_argv[] = { (char*)"woof", (char*)"-l", (char*)"error"};
-    int fake_argc = sizeof(fake_argv) / sizeof(char*);
-    parse_args(fake_argc, fake_argv);
-
+    BOOST_TEST_MESSAGE("STARTING CASE 3 ERROR");
+    char *fake_errorv[] = { (char*)"woof", (char*)"-l", (char*)"error"};
+    int fake_errorc= sizeof(fake_errorv) / sizeof(char*);
+    parse_args(fake_errorc, fake_errorv);
 
     BOOST_CHECK_EQUAL(log_global_cfg.level, LOG_ERROR);
+
+    optind = 1;
+    log_global_cfg.level_cli_override = false;
+    BOOST_TEST_MESSAGE("STARTING CASE 4 QUIET");
+    char *fake_quietv[] = { (char*)"woof", (char*)"-q", (char*)"quiet mode override, logs squelched\n"};
+    int fake_quietc= sizeof(fake_quietv) / sizeof(char*);
+    parse_args(fake_quietc, fake_quietv);
+
+    BOOST_CHECK_EQUAL(log_set_quiet, no_argument);
 
 }
 
